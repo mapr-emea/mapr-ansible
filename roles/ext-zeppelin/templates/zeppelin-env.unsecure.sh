bf9263194f57fc818238b9aa83decce87189297f
Carsten Hufe <chufe@maprtech.com>
2016-12-13 14:44:55 +0100

    autodetect versions and fixed mapr client install

--- roles/ext-zeppelin/templates/zeppelin-env.unsecure.sh
+++ roles/ext-zeppelin/templates/zeppelin-env.unsecure.sh
@@ -1,3 +1,3 @@
-export SPARK_HOME=/opt/mapr/spark/spark-{{ spark_version }}
+export SPARK_HOME={{ spark_path_result.files[0].path }}
 export HADOOP_HOME=/opt/mapr/hadoop/hadoop-{{ hadoop_version }}
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
