aec03f917e22dc7de2b031ca95857f1ae54eebf6
Carsten Hufe <chufe@maprtech.com>
2017-07-03 17:00:02 +0200

    some fixes

--- roles/mapr-hue-install/templates/create_mysql_tables.sh
+++ roles/mapr-hue-install/templates/create_mysql_tables.sh
@@ -4,9 +4,10 @@ cd {{ hue_path_result.files[0].path }}
 source ./build/env/bin/activate
 # somehow broken link on Redhat 7.3
 pip uninstall -y MySQL-python
-sleep 5
+sleep 10
 pip install MySQL-python
-sleep 5
+pip install MySQL-python
+sleep 20
 hue syncdb --noinput
 hue migrate
 deactivate
