c70637d529707973d9e0e89e0ef498366cd6e171
Carsten Hufe <chufe@maprtech.com>
2017-12-20 13:08:57 +0100

    rolling upgrade stuff

--- rolling-upgrade.sh
+++ rolling-upgrade.sh
@@ -7,5 +7,5 @@ ansible-playbook -i myhosts/hosts_5nodes helper/fetch-mapr-keys.yml
 input="myhosts/rollingnodes"
 while IFS= read -r var
 do
-  ansible-playbook -i myhosts/hosts_5nodes site-upgrade.yml --limit $var
+  ansible-playbook -i myhosts/hosts_5nodes site-upgrade.yml --limit $var -e rolling_upgrade=true
 done < "$input"
