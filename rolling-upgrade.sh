82198d4bf528413728cde06c1ab84632d74b43c5
Carsten Hufe <chufe@maprtech.com>
2018-01-16 10:25:38 +0100

    cleaning up myhosts, templates are in host_templates. removed duplication in hosts_3nodes

--- rolling-upgrade.sh
+++ rolling-upgrade.sh
@@ -4,9 +4,9 @@ set -e
 
 ansible-playbook -i myhosts/hosts_4nodes helper/fetch-mapr-keys.yml
 
-input="myhosts/rollingnodes"
+input="myhosts/rollingnodes_order"
 while IFS= read -r var
 do
-  ansible-playbook -i myhosts/hosts_4nodes site-upgrade.yml --limit $var -e rolling_upgrade=true
+  ansible-playbook -i myhosts/hosts_3nodes site-upgrade.yml --limit $var -e rolling_upgrade=true
 done < "$input"
-ansible-playbook -i myhosts/hosts_4nodes helper/set-latest-version.yml
+ansible-playbook -i myhosts/hosts_3nodes helper/set-latest-version.yml
