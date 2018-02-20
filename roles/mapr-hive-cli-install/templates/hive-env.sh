2f5250a60474e13d809875651a9134cfca99296d
Carsten Hufe <chufe@maprtech.com>
2018-02-20 10:22:51 +0100

    added Tez option to mapr-hive

--- roles/mapr-hive-cli-install/templates/hive-env.sh
+++ roles/mapr-hive-cli-install/templates/hive-env.sh
@@ -1,1 +1,6 @@
 export METASTORE_PORT=9083
+{% if hive_with_tez %}
+export TEZ_CONF_DIR={{ tez_path_result.files[0].path }}/conf
+export TEZ_JARS={{ tez_path_result.files[0].path }}/*:{{ tez_path_result.files[0].path }}/lib/*
+export HADOOP_CLASSPATH=$TEZ_CONF_DIR:$TEZ_JARS:$HADOOP_CLASSPATH
+{% endif %}
