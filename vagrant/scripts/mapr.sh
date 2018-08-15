f483fa4a092d6938996224675c853d98b639053d
Carsten Hufe <chufe@mapr.com>
2018-08-15 19:56:54 +0200

    cleanup ansible

--- vagrant/scripts/mapr.sh
+++ vagrant/scripts/mapr.sh
@@ -5,6 +5,8 @@ git clone https://github.com/mapr-emea/mapr-ansible.git
 cd mapr-ansible
 git checkout vagrant
 ansible-playbook -i vagrant/ansible_inventory/hosts_mapr_slim site-cluster.yml
+cd ~
+rm -Rf mapr-ansible
 
 maprcli volume list -json | jq '.data[].volumename' | xargs -L 1 maprcli volume modify -replication 1 -minreplication 1 -name
 maprcli volume list -json | jq '.data[].volumename' | xargs -L 1 maprcli volume modify -nsreplication 1 -nsminreplication 1 -name
