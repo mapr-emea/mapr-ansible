7032c9c030e3bab3ff7f0dcb478b4724081f2775
Carsten Hufe <chufe@mapr.com>
2018-11-07 14:14:23 +0100

    Integrated SSL certificates in normal deployment

--- helper/ssl/generate_certs.sh
+++ helper/ssl/generate_certs.sh
@@ -1,19 +1,18 @@
-#/bin/bash
+#!/bin/bash
 
 CERT_PATH=~/testsslkeys
 CLUSTER_NAME=demo.mapr.com
-
-RESULT=$(ansible -i myhosts/hosts_3nodes -m shell -a 'echo "|host=$(hostname -f)"' all)
+RESULT=$(ansible -i myhosts/hosts_3nodes -m shell -a 'echo "|host=$(hostname --ip-address)"' all)
 
 for i in $(echo $RESULT | tr "|" "\n")
 do
     if [[ "$i" =~ ^host ]]
     then
-        NODENAME=$(echo $i | cut -d "=" -f2-)
-        openssl genrsa -out $CERT_PATH/$NODENAME.key 4096
-        openssl req -new -key $CERT_PATH/$NODENAME.key -out $CERT_PATH/$NODENAME.csr -subj "/C=DE/O=MapR/OU=MapR PS/CN=$NODENAME"
-        openssl x509 -req -in $CERT_PATH/$NODENAME.csr -CA $CERT_PATH/rootCA.pem -CAkey $CERT_PATH/rootCA-private.key -CAcreateserial -out $CERT_PATH/$NODENAME.crt -days 1024 -sha256
-   #     rm -f $CERT_PATH/$NODENAME.p12
-   #     openssl pkcs12 -export -in $CERT_PATH/$NODENAME.crt -inkey $CERT_PATH/$NODENAME.key -out $CERT_PATH/$NODENAME.p12 -name $CLUSTER_NAME -password pass:mapr123
+        export NODEIP=$(echo $i | cut -d "=" -f2-)
+        export NODENUMBER=$(echo $NODEIP | cut -d "." -f4-)
+        export NODENAME=ip-10-0-0-${NODENUMBER}.ps.mapr.com
+        cat helper/ssl/cert_tpl | envsubst > $CERT_PATH/$NODENAME.conf
+        openssl req -nodes -newkey rsa:4096 -keyout $CERT_PATH/key/$NODENAME.pem -out $CERT_PATH/$NODENAME.csr -config $CERT_PATH/$NODENAME.conf
+        openssl x509 -req -in $CERT_PATH/$NODENAME.csr -CA $CERT_PATH/rootCA.pem -CAkey $CERT_PATH/rootCA-private.key -CAcreateserial -out $CERT_PATH/crt/$NODENAME.pem -days 1024 -sha256
     fi
 done
