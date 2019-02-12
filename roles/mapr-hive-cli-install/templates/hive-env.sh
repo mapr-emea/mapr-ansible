52fbe726ebc046b73fe14bcfdfb625c9fca4dbbd
Carsten Hufe <chufe@mapr.com>
2019-02-12 15:09:34 +0100

    Several Hive optimizations: - Scratch dir /tmp/hive (not /user/hive....) - Enable MapR-SASL auth when PAM is active

--- roles/mapr-hive-cli-install/templates/hive-env.sh
+++ roles/mapr-hive-cli-install/templates/hive-env.sh
@@ -4,6 +4,6 @@ export TEZ_CONF_DIR={{ tez_path_result.files[0].path }}/conf
 export TEZ_JARS={{ tez_path_result.files[0].path }}/*:{{ tez_path_result.files[0].path }}/lib/*
 export HADOOP_CLASSPATH=$TEZ_CONF_DIR:$TEZ_JARS:$HADOOP_CLASSPATH
 {% endif %}
-{% if mapr_security != 'none' %}
+{% if hive_server_security == 'pam' %}
 export HADOOP_OPTS="-Dmapr_sec_enabled=true $HADOOP_OPTS"
 {% endif %}
